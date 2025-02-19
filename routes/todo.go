b8ff7a30b358e492ac46f7560b29a1b94609f3b0
Jaxon Adams <jadamsresume1@gmail.com>
2025-02-19 01:37:33 -0700

    Add route for creating new to-do item via API

--- routes/todo.go
+++ routes/todo.go
@@ -1,19 +1,28 @@
 package routes
 
 import (
+	"fmt"
 	"go-do-something/database"
 	"go-do-something/util"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/gin-gonic/gin"
 )
 
+type NewTodoReq struct {
+	Title       string `json:"title" binding:"required"`
+	Description string `json:"description" binding:"required"`
+	DueDate     string `json:"due_date" binding: "required"`
+}
+
 type TodoListItem struct {
 	UserID      string `json:"user_id"`
 	TodoID      string `json:"todo_id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
+	Title       string `json:"title" binding:"required"`
+	Description string `json:"description" binding:"required"`
 	Status      string `json:"status"`
 	DueDate     string `json:"due_date"`
 	CreatedAt   string `json:"created_at"`
@@ -23,6 +32,11 @@ func RegisterTodoRoutes(r *gin.RouterGroup, svc *dynamodb.Client) {
 	r.GET("/todo", func(c *gin.Context) {
 		userID := "jaxontest@example.com" // TODO: add actual authentication
 		getTodoItemsByUserID(userID, svc, c)
+	})
+
+	r.POST("/todo", func(c *gin.Context) {
+		userID := "jaxontest@example.com" // TODO: add actual authentication
+		createTodoItem(userID, svc, c)
 	})
 }
 
@@ -48,4 +62,29 @@ func getTodoItemsByUserID(userID string, svc *dynamodb.Client, c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"results": todoList})
+}
+
+func createTodoItem(userID string, svc *dynamodb.Client, c *gin.Context) {
+	var json NewTodoReq
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := database.PutTodoItem(
+		svc,
+		userID,
+		strconv.FormatInt(time.Now().UnixMilli(), 10),
+		json.Title,
+		json.Description,
+		"pending",
+		json.DueDate,
+	)
+	if err != nil {
+		fmt.Printf("\nError creating new item:%v\n\n", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not create new to-do item"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": fmt.Sprintf("Successfully created item: '%v'", json.Title)})
 }
